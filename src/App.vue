fb7d8d122212108c8e61bf542c7ffc992892bdac
Denis <redman1922svap@mail.ru>
2024-01-29 23:37:59 +0300

    final layout desktop

--- src/App.vue
+++ src/App.vue
@@ -27,14 +27,74 @@
           </form>
         </div>
       </div>
-      <div></div>
+      <div class="changing-tasks-list__position">
+        <div class="changing-tasks-list__headers">
+          <h4 class="changing-tasks-list__headers-title">Задачи</h4>
+          <h4 class="changing-tasks-list__headers-title">Статус</h4>
+        </div>
+        <ul class="changing-tasks-list__ul">
+          <li class="changing-tasks-list__ul-li">
+            <p class="changing-tasks-list__ul-li-text">Сварить пельмени</p>
+            <div>
+              <span class="changing-tasks-list__ul-li-status">Открыт</span>
+            </div>
+          </li>
+          <li class="changing-tasks-list__ul-li">
+            <p class="changing-tasks-list__ul-li-text">Поднять инфрастуктуру проекта</p>
+            <div>
+              <span class="changing-tasks-list__ul-li-status">Открыт</span>
+            </div>
+          </li>
+          <li class="changing-tasks-list__ul-li">
+            <p class="changing-tasks-list__ul-li-text">
+              Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+            </p>
+            <div>
+              <span class="changing-tasks-list__ul-li-status">В работе</span>
+            </div>
+          </li>
+          <li class="changing-tasks-list__ul-li">
+            <p class="changing-tasks-list__ul-li-text">Поругаться с девопсом</p>
+            <div>
+              <span class="changing-tasks-list__ul-li-status">В работе</span>
+            </div>
+          </li>
+          <li class="changing-tasks-list__ul-li">
+            <p class="changing-tasks-list__ul-li-text">Спеть - Знаешь ли ты, вдоль ночных дорог</p>
+            <div>
+              <span class="changing-tasks-list__ul-li-status">Закрыт</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
   <div class="wrapper-view-tasks">
-    <h2>Доска задач</h2>
-    <div></div>
+    <h2 class="wrapper-view-tasks__title">Доска задач</h2>
+    <div class="view-tasks__desk">
+      <div class="view-tasks__desk-cards">
+        <span class="view-tasks__desk-cards-status">Открыто</span>
+        <ul class="view-tasks__desk-cards__list">
+          <li>Сварить пельмени</li>
+          <li>Поднять инфрастуктуру проекта</li>
+        </ul>
+      </div>
+      <div class="view-tasks__desk-cards">
+        <span class="view-tasks__desk-cards-status">В работе</span>
+        <ul class="view-tasks__desk-cards__list">
+          <li>Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу</li>
+          <li>Поругаться с девопсом</li>
+        </ul>
+      </div>
+      <div class="view-tasks__desk-cards">
+        <span class="view-tasks__desk-cards-status">Закрыто</span>
+        <ul class="view-tasks__desk-cards__list">
+          <li>Спеть - Знаешь ли ты, вдоль ночных дорог</li>
+        </ul>
+      </div>
+    </div>
   </div>
-  <footer></footer>
+  <footer class="footer"></footer>
 </template>
 
 <style lang="scss" scoped>
@@ -43,7 +103,7 @@
 
 .wrapper-changing-tasks {
   background-color: $color-gray;
-  padding: 64px 0 0 0;
+  padding: 64px 0;
 
   .changing-tasks {
     width: 100%;
@@ -220,6 +280,144 @@
         }
       }
     }
+
+    .changing-tasks-list__position {
+      background-color: $color-white;
+      padding: 40px;
+      max-width: 1200px;
+      width: 100%;
+      border-radius: 48px;
+
+      .changing-tasks-list__headers {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        margin: 0 0 24px 0;
+
+        &-title {
+          @include pt-sans-caption-text(18px);
+          line-height: 26px;
+          font-weight: 700;
+          color: $color-text;
+        }
+
+        &-title:nth-child(2) {
+          max-width: 102px;
+          width: 100%;
+          text-align: center;
+        }
+      }
+
+      .changing-tasks-list__ul {
+        &-li {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          border-bottom: 2px solid $color-border;
+          align-items: center;
+          margin: 0 0 10px 0;
+
+          &:last-child {
+            margin: 0;
+          }
+
+          &-text {
+            @include pt-sans-caption-text(16px);
+            line-height: 24px;
+            font-weight: 400;
+            color: $color-text;
+          }
+
+          div {
+            max-width: 102px;
+            width: 100%;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+          }
+
+          &-status {
+            @include pt-sans-caption-text(12px);
+            font-weight: 400;
+            line-height: 20px;
+            box-sizing: border-box;
+            padding: 5px 16px;
+            background-color: $color-gray;
+            color: $color-text;
+            border-radius: 39px;
+            margin: 0 0 5px;
+          }
+        }
+      }
+    }
   }
+}
+
+.wrapper-view-tasks {
+  width: 100%;
+  max-width: 1200px;
+  margin: 60px auto;
+
+  &__title {
+    @include montserrat-text(32px);
+    line-height: 40px;
+    color: $color-text;
+    margin: 0 0 48px;
+  }
+
+  .view-tasks__desk {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+
+    &-cards {
+      max-width: 389px;
+      width: 100%;
+
+      &-status {
+        @include pt-sans-caption-text(20px);
+        font-weight: 700;
+        line-height: 28px;
+        padding: 5px 24px;
+        background-color: $color-gray;
+        color: $color-text;
+        border-radius: 39px;
+      }
+    }
+
+    .view-tasks__desk-cards__list {
+      list-style-type: none;
+      max-width: 390px;
+      width: 100%;
+      background: $color-background-cards;
+      padding: 16px;
+      border-radius: 24px;
+      min-height: 280px;
+      margin: 24px 0 0;
+
+      li{
+        @include pt-sans-caption-text(16px);
+        font-weight: 700;
+        line-height: 24px;
+        padding: 24px 32px;
+        color: $color-text;
+        background-color: $color-white;
+        border-radius: 24px;
+        margin: 0 0 16px;
+        border: 2px solid $color-border;
+      }
+
+      li:last-child{
+        margin: 0;
+      }
+    }
+  }
+}
+
+.footer{
+  width: 100%;
+  background: $color-dark-gray;
+  height: 148px;
+  border-radius: 48px 48px 0 0;
 }
 </style>
